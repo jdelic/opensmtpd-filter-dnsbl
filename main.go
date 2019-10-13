9b082388d97377d885e5b7752001675e04f3c13b
Jonas Maurus <jonas-github@maurus.net>
2019-10-13 23:36:48 +0200

    implement concurrent queries and waiting for replies through channels

--- main.go
+++ main.go
@@ -2,15 +2,25 @@ package main
 
 import (
 	"encoding/hex"
+	"flag"
 	"fmt"
 	"github.com/jdelic/opensmtpd-filters-go"
+	"log"
 	"net"
+	"os"
 	"strings"
 )
 
 
 type DNSBLFilter struct {
 
+}
+
+
+func debug(format string, values... interface{}) {
+	if *debugOutput {
+		log.Printf(format, values)
+	}
 }
 
 
@@ -67,16 +77,75 @@ func ipToQueryPrefix(ipstr string) (string, error) {
 
 
 /* <unknown>|fail|192.168.56.162:53878|192.168.56.162:25 */
-func (d *DNSBLFilter) LinkConnect(session string, params []string) {
-	srcip := params[2]
-	if srcip[0:4] == "unix" {
-		// unix socket
+func (d DNSBLFilter) LinkConnect(session string, params []string) {
+	conn := params[2]
+	if conn[0:4] == "unix" {
+		debug("Unix socket.")
+		return
 	} else {
-		//query := ipToQueryPrefix(srcip)
+		src := strings.Split(conn, ":")
+		srcip := src[0]
+		queryPart, err := ipToQueryPrefix(srcip)
+		if err != nil {
+			debug("Debug: Error during IP processing %v\n", err)
+			opensmtpd.SoftReject(params[0], session,"Failure in IP processing")
+			return
+		}
+
+		// make a channel with enough buffer to receive all results, so we don't leak
+		// goroutines when we bail before having received all DNS results
+		repliesChan := make(chan string, len(flag.Args()))
+
+		go func(replyChan chan string) {
+			for _, host := range flag.Args() {
+				query := fmt.Sprintf("%v.%v", queryPart, host)
+
+				go func() {
+					debug("Debug: Querying %v\n", query)
+					_, err := net.LookupHost(query)
+					if err == nil {
+						replyChan <- host
+					} else {
+						debug("DNS Response from %v: %v", host, err)
+						replyChan <- ""
+					}
+				}()
+			}
+
+			for i := 0; i < len(flag.Args()); i++ {
+				reply := <- replyChan
+
+				if reply != "" {
+					opensmtpd.HardReject(params[0], session,
+						fmt.Sprintf("Your host is listed on blacklist %v", reply))
+					return  // bail after we received one positive answer
+				}
+			}
+
+			// if we have received all replies and none were true, we proceed
+			opensmtpd.Proceed(params[0], session)
+		}(repliesChan)
 	}
 }
 
 
+func Usage() {
+	fmt.Printf("Usage: %s [OPTIONS] dnsbl host 1 [dnsbl host2...]", os.Args[0])
+	flag.PrintDefaults()
+	os.Exit(1)
+}
+
+
+var debugOutput *bool
+
+
 func main() {
+	log.SetOutput(os.Stderr)
+	debugOutput = flag.Bool("debug", false, "Enable debug output")
+	flag.Usage = Usage
+	flag.Parse()
+	if flag.NArg() < 1 {
+		flag.Usage()
+	}
 	opensmtpd.Run(DNSBLFilter{})
 }
