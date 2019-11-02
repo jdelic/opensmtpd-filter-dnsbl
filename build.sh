f7f52d4947d40280f803be2f526920213b04d7e2
Jonas Maurus <jonas-github@maurus.net>
2019-11-02 21:13:32 +0100

    add simple build script

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+go build
+
+fpm \
+    -s dir \
+    -t deb \
+    -p opensmtpd-filter-dnsbl_0.1.0.deb \
+    -n opensmtpd-filter-dnsbl \
+    -v "0.1.0-0" \
+    -m "Jonas Maurus" \
+    -d "opensmtpd (>=6.6.0)" \
+    -d "opensmtpd (<<6.7)" \
+    -d "greylistd" \
+    --description "Provides integration with DNS blacklists for OpenSMTPD." \
+    --url "https://github.com/jdelic/opensmtpd-filter-dnsbl" \
+    opensmtpd-filter-dnsbl=/usr/lib/x86_64-linux-gnu/opensmtpd/filter-dnsbl
